26eabbeb60b604e902b7a98d9f53fa8da2d59b6a
matt maier <maier@users.noreply.github.com>
2017-09-28 11:28:03 -0400

    add vboxsf to /etc/modules

--- alpine3.6/scripts/90virtualbox.sh
+++ alpine3.6/scripts/90virtualbox.sh
@@ -3,3 +3,4 @@ set -eux
 echo http://dl-cdn.alpinelinux.org/alpine/v3.6/community >> /etc/apk/repositories
 apk add -U virtualbox-guest-additions virtualbox-guest-modules-hardened
 rc-update add virtualbox-guest-additions
+echo vboxsf >> /etc/modules
